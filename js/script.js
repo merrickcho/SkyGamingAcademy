11a1ac7e36f9b9763d57245053cd99132faec4fb
merrickcho <merrick.cho@gmail.com>
2023-05-17 02:49:45 -0700

    Cleaned up some lines

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-// AIzaSyA9eXX7MDgk8KsSBNuACIT07IJH8xl_YCY
-// UCHTe0Wt2h1zlFbjYfvqShpg
-
-// GET https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCHTe0Wt2h1zlFbjYfvqShpg&maxResults=10&order=date&key=AIzaSyA9eXX7MDgk8KsSBNuACIT07IJH8xl_YCY
-
-// Authorization: Bearer [YOUR_ACCESS_TOKEN]
-// Accept: application/json
-console.log("hello")
-
-fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCHTe0Wt2h1zlFbjYfvqShpg&maxResults=10&order=date&key=AIzaSyA9eXX7MDgk8KsSBNuACIT07IJH8xl_YCY")
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCHTe0Wt2h1zlFbjYfvqShpg&maxResults=1&order=date&key=AIzaSyA9eXX7MDgk8KsSBNuACIT07IJH8xl_YCY")
     .then((res)=> {
         return res.json()
 
@@ -16,18 +7,14 @@ fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=U
             let videoContainer = document.querySelector(".videoContainer")
             for(video of videos){
                 videoContainer.innerHTML += 
-                `<a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank"><img src="${video.snippet.thumbnails.medium.url}"><a>`
+                `
+                <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank"><img src="${video.snippet.thumbnails.high.url}">
+                <div class="text-light"><h4>${video.snippet.title}</h4></div>
+
+                `
                 
             }
         })
     })
-    // .then((video)=> {
-    //     // let vidThumnail = video.items.snippet.thumnails.high
-    //     // let vidId = video.items.id.videoId
-    //     // let vidTitle = video.items.snippet.title
 
-    //     console.log(video.items.snippet.title)
-    //     console.log(vidId)
-    //     console.log(vidTitle)
-    // })
 
