7c62b91fd6db56782592648a266a09eb5a73fe34
merrickcho <merrick.cho@gmail.com>
2023-05-27 16:18:43 -0700

    Made small changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCHTe0Wt2h1zlFbjYfvqShpg&maxResults=1&order=date&key=AIzaSyA9eXX7MDgk8KsSBNuACIT07IJH8xl_YCY")
+fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCHTe0Wt2h1zlFbjYfvqShpg&maxResults=3&order=date&key=AIzaSyA9eXX7MDgk8KsSBNuACIT07IJH8xl_YCY")
     .then((res)=> {
         return res.json()
 
@@ -9,7 +9,7 @@ fetch("https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=U
                 videoContainer.innerHTML += 
                 `
                 <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank"><img src="${video.snippet.thumbnails.high.url}">
-                <div class="text-light"><h4>${video.snippet.title}</h4></div>
+                <div class="text-light"><h6>${video.snippet.title}</h6></div>
 
                 `
                 
