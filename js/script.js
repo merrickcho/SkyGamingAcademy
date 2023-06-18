1c5a84cc9bccd0379972e7875821f4defe82a612
merrickcho <merrick.cho@gmail.com>
2023-06-17 18:35:19 -0700

    last push for today

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-const api_key = process.env.API_KEY
+const api_key = API_KEY
 const url = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&channelId=UCHTe0Wt2h1zlFbjYfvqShpg&maxResults=3&order=date&key=${api_key}`
 
 fetch(url)
@@ -13,9 +13,9 @@ fetch(url)
                 videoContainer.innerHTML += 
                 `
                 <div class="col"
-                    <div class="card bg-dark" style="width: 25rem">
+                    <div class="imageContainer card bg-dark" style="width: 25rem">
                         <a href="https://youtube.com/watch?v=${video.id.videoId}" target="_blank">
-                            <img src="${video.snippet.thumbnails.high.url}" class="card-img-top" alt="${video.snippet.title}>
+                            <img src="${video.snippet.thumbnails.high.url}" class="card-img-top px-2">
                         </a>
                       <div class="card-body">
                         <p class="card-text text-light mx-3">${video.snippet.title}</p>
